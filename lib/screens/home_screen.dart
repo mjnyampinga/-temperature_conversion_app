5b34b4a9467fd9fad65d7efece858ba63590b10a
nmjoselyne09 <nmjoselyne09@gmail.com>
2024-07-15 20:32:22 +0200

    Temperature Conversion

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Temperature Conversion'),
+        title: Text('Temperature Converter'),
       ),
       body: TemperatureConverter(),
     );
